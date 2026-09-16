e93841003fe7f2da49a77d17598a87e3a67e39e8
Tarun2305 <tarungopal235@gmail.com>
2026-09-16 12:32:36 +0530

    Bundle condensed headline font for consistent mobile typography

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = "reading-desk-v10";
+const CACHE_NAME = "reading-desk-v11";
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./styles.css?v=10",
+    "./styles.css?v=11",
     "./app.js?v=10",
     "./data.json",
     "./manifest.webmanifest",
     "./favicon.svg",
+    "./fonts/RobotoCondensed-Variable.ttf",
 ];
 
 self.addEventListener("install", event => {
